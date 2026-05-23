1950572efe643992e1cbf2867d9a7ba682ad7ed1
sajid yaqub <sajidmct21@gmail.com>
2026-05-22 21:42:28 -0700

    update skill section

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -7,10 +7,10 @@ function Skills() {
   return (
     <div className="bg-blue-900 text-white">
         <div className="flex flex-col justify-center items-center">
-            <h1>Skills & Technologies</h1>
-            <p className="mx-auto w-[33%] text-center">I work with modern tools and technologies to build fast, scalable and efficient web applications.</p>
+            <h1 className="text-3xl font-bold pt-23">Skills & Technologies</h1>
+            <p className="mx-auto w-[33%] text-center pt-4">I work with modern tools and technologies to build fast, scalable and efficient web applications.</p>
         </div>
-        <div className="flex flex-wrap mx-10 gap-4 justify-center">
+        <div className="flex flex-wrap mx-10 gap-x-4 gap-y-9 justify-center pt-12">
             {
                 skillData.map((item)=>(
                     <SkillCard imageurl={item.imageurl} title={item.skillTitle} description={item.skillDescription}/>
